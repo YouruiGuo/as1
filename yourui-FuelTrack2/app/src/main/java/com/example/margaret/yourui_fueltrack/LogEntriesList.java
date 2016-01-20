a2cb77c9b0f2beb5bfe9f7fe53039e7c5d954408
Yourui Guo <yourui@cs.ualberta.ca>
2016-01-20 15:13:31 -0700

    fourth edit

--- yourui-FuelTrack2/app/src/main/java/com/example/margaret/yourui_fueltrack/LogEntriesList.java
+++ yourui-FuelTrack2/app/src/main/java/com/example/margaret/yourui_fueltrack/LogEntriesList.java
@@ -1,8 +1,19 @@
 package com.example.margaret.yourui_fueltrack;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /**
  * Created by yourui on 1/19/16.
  */
-public class LogEntriesList {
+public class LogEntriesList implements Serializable {
+    private static final long serialVersionUID = 0L;
+
+    protected ArrayList<LogEntry> LogEntryList = null;
+
+    public LogEntriesList(ArrayList<LogEntry> logEntryList) {
+        LogEntryList = logEntryList;
+    }
+
 
 }
